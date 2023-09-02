1a84f207356c58e4c8b03f4953771ef77b1d56f9
Romina <rominarodriguez1500@gmail.com>
2023-09-02 17:10:11 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,4 +1,13 @@
+function cartel(){
+    alert("Hola!");
+}
+const boton = document.getElementById("miBoton");
 const miDiv = document.getElementById("miDiv");
-miDiv.addEventListener("click", function() {
+
+boton.addEventListener("click", function(event) {
+    event.stopPropagation(); 
+    notificacion(); 
+});git 
+miDiv.addEventListener("click", function(event) {
     alert("¡Hola! Soy el Div");
 });
